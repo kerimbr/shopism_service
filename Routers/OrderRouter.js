57b74a56a62924f135a6b3c8838ea934604f9a76
kerimbr <dev.kerimbora@gmail.com>
2022-05-20 19:19:55 +0300

    order create

--- Routers/OrderRouter.js
+++ Routers/OrderRouter.js
@@ -4,17 +4,17 @@ const router = express.Router();
 let config = require("../DBConfig");
 const sql = require("mssql");
 
-const {viewTableConvertToObj} = require("../utils/Utils");
+const { viewTableConvertToObj } = require("../utils/Utils");
 
 
-router.get("/:ID",async(req,res)=>{
-   try {
+router.get("/:ID", async (req, res) => {
+    try {
 
-    let id = req.params.ID;
+        let id = req.params.ID;
 
-    let pool = await sql.connect(config);
-    let query = await pool.request().query(
-        `   
+        let pool = await sql.connect(config);
+        let query = await pool.request().query(
+            `   
         SELECT 
             OD.*,
             CI.*,
@@ -40,19 +40,151 @@ router.get("/:ID",async(req,res)=>{
         INNER JOIN PaymentOptions AS PO On OD.payment_option_id = PO.payment_options_id
         WHERE OD.order_id = ${id};
         `
-      );
+        );
 
-      let viewTable = query.recordsets[0][0];
-      let orderDetail = viewTableConvertToObj(viewTable);
+        let viewTable = query.recordsets[0][0];
+        let orderDetail = viewTableConvertToObj(viewTable);
 
-      return res.send(orderDetail);
+        return res.send(orderDetail);
 
-   } catch (e) {
+    } catch (e) {
         return res.status(503).json({
             message: "API Error",
             error: e,
         });
-   }
+    }
+});
+
+router.get("/create/example", async (req, res) => {
+
+    return res.status(200).json({
+        message: "Örnek Body Gönderimi",
+        body:{
+            "userEmail": "<user_email_address>",
+            "cartItem": "<cart_item_id>",
+            "orderAddress": "<order_address_id>",
+            "deliveryId": "<delivery_id>",
+            "paymentType": "Kredi Kartı | Havale/EFT",
+            "totalPrice": "<total_price>",
+            "installment": "<installment>",
+            "installmentPrice": "<installmentPrice>"
+        }
+    });
+
+});
+
+router.post("/create", async (req, res) => {
+    try {
+
+        if (
+            req.body.userEmail === undefined || req.body.cartItem === undefined ||
+            req.body.orderAddress === undefined || req.body.deliveryId === undefined ||
+            req.body.paymentType === undefined || req.body.totalPrice === undefined ||
+            req.body.installment === undefined || req.body.installmentPrice === undefined) {
+            return res.status(404).json({
+                message: "userEmail or cartItem or orderAddress or orderStatusId or paymentType or deliveryId or totalPrice or installment or installmentPrice not be null",
+            });
+        }
+
+
+        let orderDetails = {
+            userEmail: req.body.userEmail,
+            cartItem: req.body.cartItem,
+            orderAddress: req.body.orderAddress,
+            deliveryId: req.body.deliveryId,
+            paymentType: req.body.paymentType,
+            totalPrice: req.body.totalPrice,
+            installment: req.body.installment,
+            installmentPrice: req.body.installmentPrice
+        };
+
+
+        let pool = await sql.connect(config);
+
+        let deliveryStatusQuery = await pool.request().query(
+            `   
+            INSERT INTO DeliveryStatus
+                ([delivery_status_id]
+                ,[delivery_id]
+                ,[delivery_status])
+            VALUES
+                ((SELECT MAX(delivery_status_id) FROM DeliveryStatus) + 1
+                ,${orderDetails.deliveryId}
+                ,'Kargolanmayı Bekliyor')
+            `
+        );
+
+        if (deliveryStatusQuery.rowsAffected.length === 1) {
+
+            let paymentQuery = await pool.request().query(
+                `   
+                INSERT INTO PaymentOptions
+                    (payment_options_id
+                    ,payment_type
+                    ,total_price
+                    ,installment
+                    ,installment_amount)
+                VALUES
+                    ((SELECT MAX(payment_options_id) FROM PaymentOptions) + 1
+                    ,'${orderDetails.paymentType}'
+                    ,${orderDetails.totalPrice}
+                    ,${orderDetails.installment}
+                    ,${orderDetails.installmentPrice})
+
+                `
+            );
+
+            if (paymentQuery.rowsAffected.length === 1) {
+
+                let query = await pool.request().query(
+                    `   
+                    INSERT INTO OrderDetails
+                        (order_id
+                        ,user_email
+                        ,cart_item
+                        ,order_adress
+                        ,order_status_id
+                        ,delivery_status_id
+                        ,payment_option_id
+                        ,order_date)
+                    VALUES
+                        ((SELECT MAX(order_id) FROM OrderDetails) + 1
+                        ,'${orderDetails.userEmail}'
+                        ,${orderDetails.cartItem}
+                        ,${orderDetails.orderAddress}
+                        ,2
+                        ,(SELECT MAX(delivery_status_id) FROM DeliveryStatus)
+                        ,(SELECT MAX(payment_options_id) FROM PaymentOptions)
+                        ,(SELECT CAST( GETDATE() AS datetime ))
+                        )
+    
+                    `
+                );
+
+                if(query.rowsAffected.length !== 0){
+                    return res.status(200).send("Order Created!")
+                }else{
+                    return res.status(409).send("Order Failed!");
+                }
+
+            }
+
+        } else {
+            return res.status(501).json({
+                message: "Server Error",
+                error: "deliveryStatusQuery not affected",
+            });
+        }
+
+
+
+    } catch (e) {
+        console.log(e);
+        return res.status(503).json({
+            message: "API Error",
+            error: e.toString(),
+        });
+    }
 });
 
 
